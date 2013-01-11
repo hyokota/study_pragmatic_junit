14fee220075f88b2c81153d4bf3248db2da1a1f2
hyokota <hyokota@sde.cs.titech.ac.jp>
2013-01-11 16:35:51 +0900

    static classごとにフィクスチャ生成部分を切り出し

--- src/test/java/jp/co/hyokota/tutorial/junit/enclosed/ItemStockTest.java
+++ src/test/java/jp/co/hyokota/tutorial/junit/enclosed/ItemStockTest.java
@@ -3,6 +3,7 @@ package jp.co.hyokota.tutorial.junit.enclosed;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.experimental.runners.Enclosed;
@@ -11,31 +12,40 @@ import org.junit.experimental.runners.Enclosed;
 public class ItemStockTest {
 
     public static class 商品が空の場合{
+        private ItemStock sut;
+
+        @Before
+        public void setUp(){
+            sut = new ItemStock();
+        }
+        
         @Test
         public void size_Aが0を返す() throws Exception{
-            ItemStock sut = new ItemStock();
             assertThat(sut.size("A"), is(0));
         }
 
         @Test
         public void contains_Aはfalseを返す() throws Exception{
-            ItemStock sut = new ItemStock();
             assertThat(sut.contains("A"), is(false));
         }
     }
 
     public static class 商品Aを1件含む場合{
+        private ItemStock sut;
+        
+        @Before
+        public void setUp(){
+            sut = new ItemStock();
+            sut.add("A", 1);
+        }
+
         @Test
         public void size_Aが1を返す() throws Exception{
-            ItemStock sut = new ItemStock();
-            sut.add("A", 1);
             assertThat(sut.size("A"), is(1));
         }
 
         @Test
         public void contains_Aはtrueを返す() throws Exception{
-            ItemStock sut = new ItemStock();
-            sut.add("A", 1);
             assertThat(sut.contains("A"), is(true));
         }
     }
