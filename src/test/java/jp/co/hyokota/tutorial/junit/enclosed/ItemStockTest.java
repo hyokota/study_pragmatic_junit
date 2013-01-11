544dfdc70b2538e85313ede322d92df5a1345edc
hyokota <hyokota@sde.cs.titech.ac.jp>
2013-01-11 16:32:45 +0900

    use Enclosed

--- src/test/java/jp/co/hyokota/tutorial/junit/enclosed/ItemStockTest.java
+++ src/test/java/jp/co/hyokota/tutorial/junit/enclosed/ItemStockTest.java
@@ -4,33 +4,39 @@ import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.experimental.runners.Enclosed;
 
+@RunWith(Enclosed.class)
 public class ItemStockTest {
 
-    @Test
-    public void size_Aが0を返す() throws Exception{
-        ItemStock sut = new ItemStock();
-        assertThat(sut.size("A"), is(0));
+    public static class 商品が空の場合{
+        @Test
+        public void size_Aが0を返す() throws Exception{
+            ItemStock sut = new ItemStock();
+            assertThat(sut.size("A"), is(0));
+        }
+
+        @Test
+        public void contains_Aはfalseを返す() throws Exception{
+            ItemStock sut = new ItemStock();
+            assertThat(sut.contains("A"), is(false));
+        }
     }
 
-    @Test
-    public void size_Aが1を返す() throws Exception{
-        ItemStock sut = new ItemStock();
-        sut.add("A", 1);
-        assertThat(sut.size("A"), is(1));
-    }
-    
-    @Test
-    public void contains_Aはfalseを返す() throws Exception{
-        ItemStock sut = new ItemStock();
-        assertThat(sut.contains("A"), is(false));
-    }
-    
-    @Test
-    public void contains_Aはtrueを返す() throws Exception{
-        ItemStock sut = new ItemStock();
-        sut.add("A", 1);
-        assertThat(sut.contains("A"), is(true));
-    }
+    public static class 商品Aを1件含む場合{
+        @Test
+        public void size_Aが1を返す() throws Exception{
+            ItemStock sut = new ItemStock();
+            sut.add("A", 1);
+            assertThat(sut.size("A"), is(1));
+        }
 
+        @Test
+        public void contains_Aはtrueを返す() throws Exception{
+            ItemStock sut = new ItemStock();
+            sut.add("A", 1);
+            assertThat(sut.contains("A"), is(true));
+        }
+    }
 }
