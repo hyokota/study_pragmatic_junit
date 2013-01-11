089ddf0c618848f28a607342a48e3034a14d385e
hyokota <hyokota@sde.cs.titech.ac.jp>
2013-01-11 16:25:23 +0900

    ItemStockのaddを実装

--- src/main/java/jp/co/hyokota/tutorial/junit/enclosed/ItemStock.java
+++ src/main/java/jp/co/hyokota/tutorial/junit/enclosed/ItemStock.java
@@ -1,9 +1,19 @@
 package jp.co.hyokota.tutorial.junit.enclosed;
 
-public class ItemStock {
+import java.util.HashMap;
+import java.util.Map;
 
-    public Integer size(String string) {
-        return 0;
+public class ItemStock {
+    private Map<String, Integer> stock = new HashMap<String, Integer>();
+
+    public int size(String key) {
+        if(!stock.containsKey(key)){
+            return 0;
+        }
+        return stock.get(key);
     }
 
+    public void add(String string, int i) {
+        stock.put(string, i);
+    }
 }
--- src/test/java/jp/co/hyokota/tutorial/junit/enclosed/ItemStockTest.java
+++ src/test/java/jp/co/hyokota/tutorial/junit/enclosed/ItemStockTest.java
@@ -12,4 +12,11 @@ public class ItemStockTest {
         ItemStock sut = new ItemStock();
         assertThat(sut.size("A"), is(0));
     }
+
+    @Test
+    public void size_Aが1を返す() throws Exception{
+        ItemStock sut = new ItemStock();
+        sut.add("A", 1);
+        assertThat(sut.size("A"), is(1));
+    }
 }
