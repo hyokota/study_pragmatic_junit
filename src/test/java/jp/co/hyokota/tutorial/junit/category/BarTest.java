e24d35dd1ec60682bc577ec41f99fec8635aafd4
hyokota <hyokota@sde.cs.titech.ac.jp>
2013-01-12 13:09:42 +0900

    testNameルールを

--- src/test/java/jp/co/hyokota/tutorial/junit/category/BarTest.java
+++ src/test/java/jp/co/hyokota/tutorial/junit/category/BarTest.java
@@ -3,20 +3,25 @@ package jp.co.hyokota.tutorial.junit.category;
 import jp.co.hyokota.tutorial.junit.category.categorymark.FastTest;
 import jp.co.hyokota.tutorial.junit.category.categorymark.SlowTest;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.junit.rules.TestName;
 
 public class BarTest {
 
+    @Rule
+    public TestName testName = new TestName();
+    
     @Category(FastTest.class)
     @Test
     public void fastTest(){
-        System.out.println("fastTest at BarTest");
+        System.out.println(String.format("%s at %s", testName.getMethodName(), this.getClass()));
     }
     
     @Category(SlowTest.class)
     @Test
     public void slowTest(){
-        System.out.println("slowTest at BarTest");        
+        System.out.println(String.format("%s at %s", testName.getMethodName(), this.getClass()));
     }
 }
--- src/test/java/jp/co/hyokota/tutorial/junit/category/FooTest.java
+++ src/test/java/jp/co/hyokota/tutorial/junit/category/FooTest.java
@@ -2,19 +2,24 @@ package jp.co.hyokota.tutorial.junit.category;
 
 import jp.co.hyokota.tutorial.junit.category.categorymark.FastTest;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.junit.rules.TestName;
 
 @Category(FastTest.class)
 public class FooTest {
 
+    @Rule
+    public TestName testName = new TestName();
+
     @Test
     public void test1(){
-        System.out.println("test1 at FooTest");
+        System.out.println(String.format("%s at %s", testName.getMethodName(), this.getClass()));
     }
     
     @Test
     public void test2(){
-        System.out.println("test2 at FooTest");        
+        System.out.println(String.format("%s at %s", testName.getMethodName(), this.getClass()));
     }
 }
