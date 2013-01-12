604cc17d36cf4a3a8813f67053227e637b5a844f
hyokota <hyokota@sde.cs.titech.ac.jp>
2013-01-12 12:00:00 +0900

    パラメタを複数与える。実行状況を見る為のsysoutも追加。

--- src/test/java/jp/co/hyokota/tutorial/junit/parameterize/CalculatorDatePointsTest.java
+++ src/test/java/jp/co/hyokota/tutorial/junit/parameterize/CalculatorDatePointsTest.java
@@ -14,11 +14,16 @@ public class CalculatorDatePointsTest {
     @DataPoints
     public static Fixture[] PARAMs = {
       new Fixture(3, 4, 7),  
+      new Fixture(1, 2, 3),  
+      new Fixture(4, 5, 9),  
+      new Fixture(1, 1, 2),  
+      new Fixture(2, -1, 1),  
     };
     
     @Theory
     public void addTest(Fixture p){
         Calculator calc = new Calculator();
+        System.out.println(p);
         assertThat(calc.add(p.x, p.y), is(p.expected));
     }
     
@@ -31,6 +36,11 @@ public class CalculatorDatePointsTest {
             this.x = x;
             this.y = y;
             this.expected = expected;
+        }
+        
+        @Override
+        public String toString(){
+            return String.format("x: %d, y: %d, expected: %d", x, y, expected);
         }
     }
 }
