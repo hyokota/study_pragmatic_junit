bf934be0a943134b6f109f76701bcd7e1746d6f6
hyokota <hyokota@sde.cs.titech.ac.jp>
2013-01-12 11:56:06 +0900

    Fixtureをパラメタとして受け取るテストの作成

--- src/test/java/jp/co/hyokota/tutorial/junit/parameterize/CalculatorDatePointsTest.java
+++ src/test/java/jp/co/hyokota/tutorial/junit/parameterize/CalculatorDatePointsTest.java
@@ -3,16 +3,34 @@ package jp.co.hyokota.tutorial.junit.parameterize;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
-import org.junit.Test;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 
+@RunWith(Theories.class)
 public class CalculatorDatePointsTest {
 
-    @Test
-    public void addTest(){
-        int x = 3;
-        int y = 4;
-        int expected = 7;
+    @DataPoints
+    public static Fixture[] PARAMs = {
+      new Fixture(3, 4, 7),  
+    };
+    
+    @Theory
+    public void addTest(Fixture p){
         Calculator calc = new Calculator();
-        assertThat(calc.add(x, y), is(expected));
+        assertThat(calc.add(p.x, p.y), is(p.expected));
+    }
+    
+    static class Fixture{
+        int x;
+        int y;
+        int expected;
+
+        Fixture(int x, int y, int expected){
+            this.x = x;
+            this.y = y;
+            this.expected = expected;
+        }
     }
 }
